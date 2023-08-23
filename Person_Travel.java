293ab75a9fa5211aa5d68b0e9328c4c42fd87e35
Chandan2024 <chandanw03@gmail.com>
2023-08-23 13:24:59 +0530

    Problem 1 Solved

---
+++ Person_Travel.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Person_Travel {
+    public static void main(String args[]) {
+        Scanner z = new Scanner(System.in);
+        int total, hour, min,res_hour;
+        float res_min, final_time, out;
+
+        System.out.println("Enter hours: ");
+        hour = z.nextInt();
+        System.out.println("Enter minute: ");
+        min = z.nextInt();
+
+        total = (hour * 60) + min;
+        out = 1440 - total;
+
+        final_time = out / 60;
+        res_hour = (int) final_time;
+        res_min = final_time - res_hour;
+       float op_min = res_min * 60;
+       res_min=(int)op_min;
+
+        // System.out.println("Hour :" + res_hour + " Min :" + op_min);
+        System.out.println(res_hour+"::"+op_min);
+        z.close();
+    }
+}
