f65b322c2a848e81aad8912ca36e8eb0b156f1f7
Chandan2024 <chandanw03@gmail.com>
2023-08-23 14:51:36 +0530

    Second Solved

--- Person_Travel.java
+++ Person_Travel.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Person_Travel {
     public static void main(String args[]) {
         Scanner z = new Scanner(System.in);
-        int total, hour, min,res_hour;
+        int total, hour, min, res_hour;
         float res_min, final_time, out;
 
         System.out.println("Enter hours: ");
@@ -17,11 +17,11 @@ class Person_Travel {
         final_time = out / 60;
         res_hour = (int) final_time;
         res_min = final_time - res_hour;
-       float op_min = res_min * 60;
-       res_min=(int)op_min;
+        float op_min = res_min * 60;
+        int minute = (int) op_min;
 
-        // System.out.println("Hour :" + res_hour + " Min :" + op_min);
-        System.out.println(res_hour+"::"+op_min);
+        // System.out.println("Hour :" + res_hour + " Min :" + minute);
+        System.out.println(res_hour+"::"+minute);
         z.close();
     }
 }
